f0f333755171a9a73616a3b4b0a46b2c02f08033
Evan Conway <p4r4n01d42@gmail.com>
2013-10-16 17:00:23 +1100

    Add Gradle Support; Target API 18
    Add Gradle build files
    Target API 18 (Android 4.3)
    Check API version before setting threadPolicy (if debug enabled)

--- src/org/jtb/alogcat/ALogcatApplication.java
+++ src/org/jtb/alogcat/ALogcatApplication.java
@@ -1,15 +1,20 @@
 package org.jtb.alogcat;
 
+import android.annotation.TargetApi;
 import android.app.Application;
+import android.os.Build;
 import android.os.StrictMode;
 
 public class ALogcatApplication extends Application {
 	private static final boolean DEBUG = false;
 
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		if (DEBUG) {
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (DEBUG && currentapiVersion >= android.os.Build.VERSION_CODES.GINGERBREAD) {
 			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
 					.detectAll().penaltyLog().build());
 			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
