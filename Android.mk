7bb1b2a156f67ba54302034b55a38bc45d5cf156
Jaiglissechef-i9100 <yaggiland@hotmail.com>
2014-01-20 18:28:19 +0100

    Rebase

--- Android.mk
+++ Android.mk
@@ -1,10 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := aLogcat
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_SDK_VERSION := current
+
+LOCAL_PACKAGE_NAME := alogcat
 
 include $(BUILD_PACKAGE)
